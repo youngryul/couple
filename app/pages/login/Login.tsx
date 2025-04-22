bea76c6f642a0e7f06a83f9521d2cc32a9932210
PNS212624\Hansol <yrlee00@hansol.com>
2025-04-22 15:37:27 +0900

    feat: 로그인 페이지 퍼블리싱 완료

--- app/pages/login/Login.tsx
+++ app/pages/login/Login.tsx
@@ -1,4 +1,26 @@
-export default function Login() {
-    return <div>1</div>
-};
+import { Button } from "~/components/Button";
 
+export default function Login() {
+  return (
+    <div className="h-full w-full flex flex-col items-center justify-center gap-8">
+      <img src="app/assets/loginImg.png" />
+      <div className="flex flex-col gap-4 w-full px-8">
+        <Button className="relative bg-[#02C759]">
+          <img
+            className="absolute left-4 w-10 h-10"
+            src="app/assets/naver.png"
+          />
+          네이버로 로그인
+        </Button>
+        <Button className="relative bg-[#FEE500] text-[#000000]">
+          <img
+            className="w-10 h-10 absolute left-4"
+            src="app/assets/kakao.png"
+          />
+          카카오톡으로 로그인
+        </Button>
+        <Button>하루밍으로 로그인</Button>
+      </div>
+    </div>
+  );
+}
