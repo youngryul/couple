330d91d704bd4c1c784671ea3c158ea1b0837133
PNS212624\Hansol <yrlee00@hansol.com>
2025-05-14 16:55:54 +0900

    feat: 스토리 페이징 처리 / home 화면 연결 및 생성

---
+++ app/pages/home/Home.tsx
@@ -0,0 +1,9 @@
+import { Button } from "~/components/Button";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
+export default function Home() {
+  return (
+    <div className="h-full w-full flex flex-col items-center justify-center gap-8"></div>
+  );
+}
--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("pages/main/Main.tsx"),
   route("/login", "pages/login/Login.tsx"),
+  route("/story", "pages/story/Story.tsx"),
+  route("/home", "pages/home/Home.tsx"),
 ] satisfies RouteConfig;
