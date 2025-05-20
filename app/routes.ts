3ac3451a113fb69db888c3f27b4afbf8114a1112
PNS212624\Hansol <yrlee00@hansol.com>
2025-05-20 13:30:11 +0900

    feat: 하루밍 로그인 연동 (니꼬 supaplate 연동)

--- app/routes.ts
+++ app/routes.ts
@@ -2,8 +2,9 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("pages/main/Main.tsx"),
-  route("/login", "pages/login/Login.tsx"),
+  route("/login", "pages/login/LoginView.tsx"),
   route("/story", "pages/story/Story.tsx"),
   route("/home", "pages/home/Home.tsx"),
   route("/calendar", "pages/calendar/CalendarView.tsx"),
+  route("/login/haru", "pages/login/components/Login.tsx"),
 ] satisfies RouteConfig;
