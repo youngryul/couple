9b98a7b757104988e61cbf855be799177ff4b9f0
PNS212624\Hansol <yrlee00@hansol.com>
2025-06-17 08:52:29 +0900

    fix: 일기 db 컬럼 수정

--- app/pages/diary/schema.ts
+++ app/pages/diary/schema.ts
@@ -7,12 +7,12 @@ export const coupleYn = pgEnum("couple_yn", COUPLE_YN);
 
 // 테이블 정의
 export const diaries = pgTable("diary_main", {
-  diaryId: varchar("diary_id", { length: 30 }).primaryKey(), // 일기 ID
-  inputId: varchar("input_id", { length: 30 }).notNull(), // 작성자
-  inputDt: date("input_dt").notNull(), // 작성일시
-  moodType: moodTypes("mood_type"), // 기분 상태
-  content: varchar("content", { length: 500 }), // 일기 내용
-  fileId: varchar("file_id", { length: 30 }), // 이미지 첨부
-  coupleYn: coupleYn("couple_yn"), // 커플 공개 여부
-  updateDt: date("update_dt"), // 수정일시
+  diary_id: varchar({ length: 30 }).primaryKey(), // 일기 ID
+  input_id: varchar({ length: 30 }).notNull(), // 작성자
+  input_dt: date().notNull(), // 작성일시
+  mood_type: moodTypes("mood_type"), // 기분 상태
+  content: varchar({ length: 500 }), // 일기 내용
+  file_id: varchar({ length: 30 }), // 이미지 첨부
+  couple_yn: coupleYn(), // 커플 공개 여부
+  update_dt: date(), // 수정일시
 });
